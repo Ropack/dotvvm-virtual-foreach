8c3dd6c1cb960e2993f88f48ca76c33632542636
Radek Rous <radek.rous@riganti.cz>
2020-05-06 12:27:32 +0200

    Add utility function for adding rows to viewmodel

--- scripts/data.js
+++ scripts/data.js
@@ -2,15 +2,22 @@
 function generateRows(count) {
     var rows = [];
     for (var i = 0; i < count; i++) {
-        rows.push({
-            Id: ko.observable(i),
-            FirstName: ko.observable("First " + i),
-            LastName: ko.observable("Last " + i)
-        });
+        rows.push(getRow(i));
     }
     return rows;
 }
+function getRow(index) {
+    return {
+        Id: ko.observable(index),
+        FirstName: ko.observable("First " + index),
+        LastName: ko.observable("Last " + index)
+    };
+}
 var testViewModel = {
-    GridData: ko.observableArray(generateRows(500))
+    GridData: ko.observableArray(generateRows(500)),
+    addNewRow: function () {
+        var dataLength = this.GridData().length;
+        this.GridData.push(getRow(dataLength));
+    }
 };
 ko.applyBindings(testViewModel);
--- scripts/data.ts
+++ scripts/data.ts
@@ -4,18 +4,25 @@ function generateRows(count: number) {
     const rows = [];
 
     for (let i = 0; i < count; i++) {
-        rows.push({
-            Id: ko.observable(i),
-            FirstName: ko.observable("First " + i),
-            LastName: ko.observable("Last " + i)
-        });
+        rows.push(getRow(i));
     }
 
     return rows;
 }
 
+function getRow(index: number): any {
+    return { 
+        Id: ko.observable(index), 
+        FirstName: ko.observable("First "+ index), 
+        LastName: ko.observable("Last "+ index)}
+}
+
 var testViewModel = {
-    GridData: ko.observableArray(generateRows(500))
+    GridData: ko.observableArray(generateRows(500)),
+    addNewRow():void {
+        let dataLength = this.GridData().length;
+        this.GridData.push(getRow(dataLength));
+    }
 };
 
 ko.applyBindings(testViewModel);
